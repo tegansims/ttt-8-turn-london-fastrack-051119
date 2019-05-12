a4a3908925330f4ec3aa1ba026fc39375e50ee8e
Tegan Sims <tegansims@hotmail.com>
2019-05-12 11:36:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,8 +18,8 @@ end
 
 def valid_move(board,answer)
   puts "What position would you like to play? Please choose 1-9."
-  answer = gets.strip
-  answer < 9 && answer >=0
+  answer1 = gets.strip
+  answer1 < 9 && answer >=0
 end
 
 def position_taken(board, answer)
