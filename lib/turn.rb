79757ee8c230c46d7cbf88f5f5709211acb08d9a
Tegan Sims <tegansims@hotmail.com>
2019-05-12 10:58:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,14 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
 def position
   puts "What position would you like to play? Please choose 1-9."
   answer = gets.strip
